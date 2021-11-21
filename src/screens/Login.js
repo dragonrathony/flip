9d4b9d54ba1425a5331bb5a6b076e94597635a93
dragonrathony <peacehoney425@gmail.com>
2021-11-20 23:06:37 -0500

    create login, home page

---
+++ src/screens/Login.js
@@ -0,0 +1,16 @@
+// Login.js
+
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export class Login extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Login</Text>
+            </View>
+        )
+    }
+}
+
+export default Login;
